434965a4462483e2aaa0cd0c740436399b3e9dbb
unknown <wintron@(none)>
2012-01-23 11:03:05 +0100

    #497

---
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/NotWordBoundaryTransitionLabel.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.amu.wmi.daut.base;
+
+/**
+ * Klasa przejścia, które odpowiada operatorowi \B z wyrażeń regularnych
+ */
+public class NotWordBoundaryTransitionLabel extends ZeroLengthConditionalTransitionLabel {
+
+    @Override
+    protected boolean doCheckContext(String s, int position)
+    {
+
+        if (s.length() < position || position < 0)
+             throw new PositionOutOfStringBordersException();
+
+        if (position == s.length())
+            return false;
+
+        if (position == 0)
+            return false;
+
+        return true;
+    };
+
+    @Override
+    public boolean canAcceptCharacter(char c) {
+        return false;
+    };
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "NotWordBoundary";
+    }
+
+}
+
