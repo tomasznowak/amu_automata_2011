f7d934545c0567200628e2c67ad97826ea65b1ae
Jakub Kubacki <kuba@kuba.(none)>
2012-01-24 17:55:25 +0100

    greibach1

--- daut-re/src/main/java/pl/edu/amu/wmi/daut/re/GrammarUtils.java
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/GrammarUtils.java
@@ -26,4 +26,28 @@ public class GrammarUtils {
         }
         return true;
     }
+    
+    /**
+     * Metoda sprawdzająca, czy podana gramatyka jest w postaci normalnej
+     * Greibach.
+     */
+    public boolean isGreibach(Grammar g) {
+        List<GrammarRule> rules = g.allRules();
+        for (GrammarRule rule : rules) {
+            if (rule.getArity() == 0
+                    || !(rule.getRhsFirstSymbol().isTerminalSymbol())  
+               ) {
+                return false;
+            }
+            else {
+                List<GrammarSymbol> rhsSymbols = rule.getRhsSymbols();
+                for (int i = 1; i < rhsSymbols.size(); i++) {
+                    if (rhsSymbols.get(i).isTerminalSymbol()) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
 }
