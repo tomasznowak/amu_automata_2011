59c34893a7946d50907946ea4df7682d1e34e3b8
none <none>
2012-01-25 01:03:08 +0100

    Testy

---
+++ daut-re/src/test/java/pl/edu/amu/wmi/daut/re/TestGrammarUtils.java
@@ -0,0 +1,58 @@
+/*
+package pl.edu.amu.wmi.daut.re;
+
+import java.util.ArrayList;
+import java.util.List;
+import junit.framework.TestCase;
+*/
+//public class TestGrammarUtils extends TestCase {
+/**
+* Test sprawdzający działanie metody isChomsky(). 
+*/
+  /*public final void testIsChomsky() {
+
+        GrammarNonterminalSymbol startSymbol = new GrammarNonterminalSymbol('S');
+        GrammarNonterminalSymbol symbolA = new GrammarNonterminalSymbol('A');
+        GrammarNonterminalSymbol symbolB = new GrammarNonterminalSymbol('B');
+        GrammarNonterminalSymbol symbolC = new GrammarNonterminalSymbol('C');
+
+        GrammarTerminalSymbol symbola = new GrammarTerminalSymbol('a');
+        GrammarTerminalSymbol symbolb = new GrammarTerminalSymbol('b');
+        GrammarTerminalSymbol symbolc = new GrammarTerminalSymbol('c');
+        
+        Grammar grammar = new Grammar(startSymbol);
+        
+        List<GrammarSymbol> tmp = new ArrayList<GrammarSymbol>();
+        tmp.add(symbolA);
+        tmp.add(symbolB);
+        tmp.add(symbolC);
+
+        grammar.addRule(new GrammarRule(startSymbol, tmp));
+        grammar.addRule(new GrammarRule(symbolA, symbola));
+        grammar.addRule(new GrammarRule(symbolB, symbolb));
+        grammar.addRule(new GrammarRule(symbolC, symbolc));
+        
+        assertFalse(GrammarUtils.isChomsky(grammar));
+        
+        GrammarNonterminalSymbol startSymbol2 = new GrammarNonterminalSymbol('S');
+        GrammarNonterminalSymbol symbolA2 = new GrammarNonterminalSymbol('A');
+        GrammarNonterminalSymbol symbolB2 = new GrammarNonterminalSymbol('B');
+
+        GrammarTerminalSymbol symbola2 = new GrammarTerminalSymbol('a');
+        GrammarTerminalSymbol symbolb2 = new GrammarTerminalSymbol('b');
+        
+        Grammar grammar2 = new Grammar(startSymbol2);
+        
+        List<GrammarSymbol> tmp2 = new ArrayList<GrammarSymbol>();
+        tmp.add(symbolA2);
+        tmp.add(symbolB2);
+
+        grammar2.addRule(new GrammarRule(startSymbol2, tmp2));
+        grammar2.addRule(new GrammarRule(symbolA2, symbola2));
+        grammar2.addRule(new GrammarRule(symbolB2, symbolb2));
+
+        assertTrue(GrammarUtils.isChomsky(grammar2));
+    }
+}*/
+
+       
